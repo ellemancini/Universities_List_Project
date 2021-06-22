1c4c1a7500bd33e9ed704c611de10f15e98b4681
ellemancini <83850228+ellemancini@users.noreply.github.com>
2021-06-22 00:39:04 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Universities_List_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
