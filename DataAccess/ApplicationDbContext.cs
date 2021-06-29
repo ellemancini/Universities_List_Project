c54ba2a3a69081b5f89dc912d8b7489be84fe534
ellemancini <83850228+ellemancini@users.noreply.github.com>
2021-06-29 17:56:59 -0400

    Changes

--- DataAccess/ApplicationDbContext.cs
+++ DataAccess/ApplicationDbContext.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Universities_List_Project.Models;
+
+namespace Universities_List_Project.DataAccess
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<University> University { get; set; }
+    }
+
+}
